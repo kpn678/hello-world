9f22f1f393ba2b0c3a96b7203bb4afd4cdd0a5ad
Kristy Nguyen <kristypnguyen7@gmail.com>
2022-12-30 13:35:21 -0700

    Create and pass down username and room state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import Home from './Components/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import { io } from 'socket.io-client';
+import Home from './Components/Home';
 
 function App() {
+
+  const [username, setUsername] = useState('');
+  const [room, setRoom] = useState('');
+
   return (
     <Router>
       <main>
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route 
+            path='/' 
+            element={
+              <Home 
+                setUsername={setUsername}
+                username={username}
+                setRoom={setRoom}
+                room={room}
+              />
+            } 
+          />
         </Routes>
       </main>
     </Router>
