8872725c5b8b249a7fad42e203e707a15bc79e5e
Kristy Nguyen <kristypnguyen7@gmail.com>
2023-01-10 16:43:00 -0700

    Create sendMessage fn

--- src/Components/MessageInput.js
+++ src/Components/MessageInput.js
@@ -1,7 +1,16 @@
 import './MessageInput.css';
+import { useState } from 'react';
 
-const MessageInput = () => {
+const MessageInput = ({ username, room, socket}) => {
+  const [message, setMessage] = useState('');
 
+  const sendMessage = () => {
+    if (message !== '') {
+      const __createdtime__ = Date.now();
+      socket.emit('send_message', { username, room, message, __createdtime__ });
+      setMessage('');
+    }
+  }
 }
 
 export default MessageInput;
