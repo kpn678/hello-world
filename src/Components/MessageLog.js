9a897e485403ef41e01665b7014172b567c4c285
Kristy Nguyen <kristypnguyen7@gmail.com>
2023-01-10 13:31:50 -0700

    Create messagesReceived state

--- src/Components/MessageLog.js
+++ src/Components/MessageLog.js
@@ -1,7 +1,8 @@
 import './MessageLog.css';
+import { useState } from 'react';
 
 const Messages = () => {
-
+  const [messagesReceived, setMessagesReceived] = useState([]);
 }
 
 export default Messages;
