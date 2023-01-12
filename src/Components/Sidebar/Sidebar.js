08f9b6a485fc00ed729c5c574f45aa4940c845e2
Kristy Nguyen <kristypnguyen7@gmail.com>
2023-01-12 15:47:52 -0700

    Create useEffect for retrieving users in room

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -3,9 +3,16 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Sidebar = ({ username, room, socket }) => {
-  const [roomUsers, setRoom] = useState([]);
+  const [roomUsers, setRoomUsers] = useState([]);
 
   const navigate = useNavigate();
+
+  useEffect(() => {
+    socket.on('chatroom_users', (data) => {
+      setRoomUsers(data);
+    });
+    return () => socket.off('chatroom_users');
+  }, [socket]);
 }
 
 export default Sidebar;
